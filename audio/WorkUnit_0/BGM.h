5c4c552b4d697c7506bf82fc0523b7ae99d3770b
ricky10099 <rickyto10099@gmail.com>
2024-12-20 08:21:10 +0900

    Added CRI library

---
+++ audio/WorkUnit_0/BGM.h
@@ -0,0 +1,27 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : ADX test
+ *  Tool Ver.        : Ver.3.52.03
+ *  ACB  Ver.        : Ver.1.42.1
+ *  File Path        : H:/CRIWARE/CriAtomCraft/projects/ADX test/Public/WorkUnit_0
+ *  File Name        : BGM.h
+ *  File Size        : 7015520 bytes
+ *  Date Time        : 2024/12/20 01:32:52
+ *  Target           : Public
+ *  Cues             : 2
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : H:/CRIWARE/CriAtomCraft/projects/ADX test/Public/WorkUnit_0/BGM.awb
+ *===========================================================================*/
+
+#define CRI_BGM_CUENUM (2)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_BGM_W006                 ( 0) /*  */
+#define CRI_BGM_KS039                ( 1) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
